c8c9431e0b89939fd9ebf88e9c774f1e72bc1d66
harshit sinha <coollumia@outlook.com>
2017-01-23 02:54:03 +0530

    key fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: '3b337084-3428-4878-95f5-bf643ab0f709', appPassword: '8oP87QddbszAm4PtVBNKwGg' }); 
+({ appId: '3b337084-3428-4878-95f5-bf643ab0f709', appPassword: 'SLZ3JikKJRBSiGYqMXG5Pn4' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
