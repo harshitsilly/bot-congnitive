99615d91564d7987849aa47d948d701a098e264c
harshit sinha <coollumia@outlook.com>
2017-01-23 02:35:19 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: '3b337084-3428-4878-95f5-bf643ab0f709', appPassword: '8oP87QddbszAm4PtVBNKwGg' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
